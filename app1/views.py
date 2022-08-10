06617ee27887bc01b842bd64a3f05d10b25ef284
Christo77793 <christo77793@gmail.com>
2022-08-10 23:45:14 +0530

    Changes reverted

--- app1/views.py
+++ app1/views.py
@@ -8,34 +8,23 @@ import matplotlib.pyplot as plt, mpld3
 
 from calendar import HTMLCalendar
 from datetime import datetime
-
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly
+import json
+import requests as req
 
 # BOOTSTRAP
 # https://getbootstrap.com/docs/5.0/getting-started/introduction/
 
 def home(request):
     project_name = "Data Programming Final Project"
-
-    df = pd.read_csv('https://raw.githubusercontent.com/Christo77793/Data-Sets/master/api_data.csv')
-
-    df1 = pd.Series(df['velocity'].values, index=df['true_track']).to_dict()
-    dfx = pd.Series(df['velocity'].values, index=df['true_track'])
-    df2 = pd.Series(df['longitude'].values, index=df['latitude']).to_dict()
-
-    data1 = {}
-    data1.update(dfx.head(10))
-
-    data2 = {"Longitude": "Latitude"}
-    data2.update(df2)
-
-    return render(
-        request,
-        "app1/home.html",
-
-        {
-            "project_name": project_name,
-            "data1": data1,
-            "data2": data2,
-        }
-
-    )
+    response = req.get(r'https://atcapi20220809022704.azurewebsites.net/AtcDetails/')
+    data = response.text
+    df = pd.read_json(data, orient='records')    
+    fig = px.scatter_polar(theta=df['trueTrack'], r=df['velocity'], template="plotly_dark",color=df['trueTrack'])
+    fig.update_layout(autosize=True, hovermode='closest')    
+    g1json = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
+    fig1 = px.density_mapbox(lat=df['latitude'] ,lon=df['longitude'] ,radius=1,mapbox_style='stamen-terrain',zoom=0.75,center=dict(lat=0,lon=0))
+    g2json = json.dumps(fig1,cls=plotly.utils.PlotlyJSONEncoder)       
+    return render(request,"app1/home.html", {"g1json":g1json,"g2json":g2json})
